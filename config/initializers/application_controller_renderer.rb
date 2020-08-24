ba4bd9a2dba91cd063f0a6085cd3ee7b876526e7
松永実華 <mika@matsunagamikanoMacBook-Pro.local>
2020-08-24 14:25:05 +0900

    [Add]画像投稿機能

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '0ab2275cead63894d7de1548d1ae93c39005204974841115c8afc0c1c150cb632b30669f4714bc35d6e66ba3a9e01061f9a04ba122603779bc31d7f0ba7e1c26'
